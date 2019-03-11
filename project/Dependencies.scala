49eb2394700c19bd076917a575a32f2107525ee3
Jon <dyercode@users.noreply.github.com>
2019-03-11 18:50:53 -0400

    new scalatest version.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   // Versions
 
   // Libraries
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.6"
 
   // Projects
   val skeleDependencies: Seq[ModuleID] = Seq(scalaTest % Test)
